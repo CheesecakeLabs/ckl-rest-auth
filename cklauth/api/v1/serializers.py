dd42bf3aaca122d4105f494a5065eddd738e0d76
Rodolfo Lottin Pereira <rodolfolottin1@gmail.com>
2017-12-28 18:01:49 -0200

    Feat: Add register endpoint

--- cklauth/api/v1/serializers.py
+++ cklauth/api/v1/serializers.py
@@ -1,6 +1,37 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from rest_framework.authtoken.models import Token
 
 
-class TokenSerializer(serializers.Serializer):
-    pass
+class RegisterSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        required=True,
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+                message='This username is already in use.'
+            )
+        ]
+    )
+    email = serializers.EmailField(
+        required=True
+    )
+    password = serializers.CharField(
+        required=True
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'email',
+            'password'
+        )
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        token = Token.objects.create(user=user)
+
+        return user, token
 
--- cklauth/api/v1/views.py
+++ cklauth/api/v1/views.py
@@ -1,13 +1,27 @@
 from django.http import JsonResponse
-from django.contrib.auth import authenticate
-
-from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
+
+from .serializers import RegisterSerializer
 
 
 @api_view(['POST',])
 def register(request):
-    return JsonResponse({})
+    data = JSONParser().parse(request)
+    serializer = RegisterSerializer(data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+        return JsonResponse({
+            'message': 'Ok.'
+        }, status=status.HTTP_201_CREATED)
+
+    return JsonResponse({
+        'message': serializer.errors
+    }, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['POST',])
