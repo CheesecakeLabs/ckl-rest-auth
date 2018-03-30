b768e79f60a0dd709b7b088b8c1ffab12ce0f818
Pedro <pedrosettiinoue@gmail.com>
2018-03-30 12:09:43 -0300

    Add SocialAccount to django admin

--- cklauth/admin.py
+++ cklauth/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.urls import reverse
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.SocialAccount)
+class SocialAccountAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+    )
+    readonly_fields = (
+        'user_',
+        'facebook_id',
+        'google_id',
+    )
+    exclude = (
+        'user',
+    )
+    search_fields = (
+        'user__username',
+    )
+
+    def user_(self, instance):
+        if not instance.user:
+            return '-'
+        return '<a href="{0}">{1}</a>'.format(
+            reverse('admin:auth_user_change', args=[instance.user.id]),
+            instance.user.username or instance.user.id
+        )
+
+    user_.allow_tags = True
