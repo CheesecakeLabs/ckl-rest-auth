1025d6feab09017b0da22bec0e205c84a4838d9d
Bernardo Smaniotto <be.smaniotto@gmail.com>
2018-04-12 14:15:36 -0300

    Style: remove unused import on testapp views

--- testapp/testapp/views.py
+++ testapp/testapp/views.py
@@ -1,16 +1,10 @@
-import requests
-
-from rest_framework.views import APIView
 from django.http import JsonResponse
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework import status
-from django.shortcuts import redirect
-
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
+
 from cklauth.auth import TokenAuthSupportQueryString
+
 
 @api_view(['GET',])
 @authentication_classes((TokenAuthSupportQueryString,))
